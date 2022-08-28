bdf2c4932d93256bb9b40397a9ea26fb727261f2
Ramu Pendyala <pendyalajmr@yahoo.com>
2022-08-28 16:53:11 +0530

    Modified class

--- advpython/inheritance-intro.py
+++ advpython/inheritance-intro.py
@@ -1,12 +1,21 @@
 #Inheritance
-
 class Decimal:
 
-    def __init__(self, number, precision):
+    def __init__(self,  number, precision):
         self.number = number
         self.precision = precision
 
     def __repr__(self):
-        return "%.2f" % self.number
+        return "%.{}f".format(self.precision) % self.number
+
+class Currency(Decimal):
+    def __init__(self, symbol, number, precision):
+        super().__init__(number, precision)
+        self.symbol = symbol
+
+    def __repr__(self):
+        return "{}{}".format(self.symbol, super().__repr__())
+
 
 print(Decimal(48.5858588585858, 2))
+print(Currency('Rs.', 49.5858588585858, 1))
